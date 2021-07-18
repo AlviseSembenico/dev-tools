be25e8601a4e1d0179661e35192dcee596b84f1c
AlviseSembenico <alvise.sembenico@student.uva.nl>
2021-07-18 22:38:15 +0200

    execute on the fly

--- API/src/routers/executor.py
+++ API/src/routers/executor.py
@@ -1,15 +1,22 @@
-import tempfile
+import uuid
 import subprocess
 
 from fastapi import APIRouter, HTTPException
+from pydantic.main import BaseModel
 
 from ..internal.interfaces import MONGO_CLIENT
 
 
-router = APIRouter(prefix='/api')
+router = APIRouter(prefix='/api/snippets')
 
 
-@router.post('/run/{id}')
+class Snippet(BaseModel):
+
+    code: str
+    language: str
+
+
+@router.post('/execute/{id}')
 def run(id: str):
     snippet = MONGO_CLIENT.devtools.snippets.find_one({"_id": id})
     if snippet is None:
@@ -19,6 +26,19 @@ def run(id: str):
     with open(filename, 'w') as f:
         f.write(snippet['code'])
     result = subprocess.run(['python', filename], stdout=subprocess.PIPE)
+    return {
+        'res': result.stdout,
+        'error': result.stderr
+    }
+
+
+@router.post('/execute')
+def run(snippet: Snippet):
+    filename = f'../execute_{uuid.uuid4()}.py'
+    with open(filename, 'w') as f:
+        f.write(snippet.code)
+    result = subprocess.run(['python', filename],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return {
         'res': result.stdout,
         'error': result.stderr
