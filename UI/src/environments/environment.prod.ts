84d71c31cb136a89871ff81e2d9f1ba907054694
AlviseSembenico <alvise.sembenico@student.uva.nl>
2021-07-20 18:16:43 +0200

    add production environment

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://v6rskcydid.execute-api.eu-central-1.amazonaws.com/dev/api',
+  authUrl: 'https://n1gr5tz4m4.execute-api.eu-central-1.amazonaws.com/dev/'
 };
